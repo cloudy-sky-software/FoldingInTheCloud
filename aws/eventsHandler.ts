cffc84be80d533bbf59874e2b099fa00dcb5f8de
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-02 10:19:07 -0700

    Add ec2:DescribeNetworkInterfaces as well.

--- aws/eventsHandler.ts
+++ aws/eventsHandler.ts
@@ -72,6 +72,7 @@ export class EventsHandler extends pulumi.ComponentResource {
                         "events:DeleteRule",
 
                         "ec2:CreateNetworkInterface",
+                        "ec2:DescribeNetworkInterfaces",
                         "ec2:DescribeSpotInstanceRequests",
                         "ec2:DescribeInstances",
                         "ec2-instance-connect:SendSSHPublicKey",
