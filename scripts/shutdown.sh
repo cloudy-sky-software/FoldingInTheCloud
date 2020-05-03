0c0c5c0d6e8f630388e6b6fc3a348e2b07c56e79
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-03 13:26:04 -0700

    Improve shutdown script.

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
->/dev/null 2>/dev/null which FAHClient || {
-    echo "FAHClient is not installed."
-    exit 0
-}
+IS_ACTIVE=$(systemctl is-active FAHClient)
+if [ "${IS_ACTIVE}" = "active" ]; then
+    echo "Stopping the FAHClient service..."
+    sudo /etc/init.d/FAHClient stop
+fi
 
-echo "Finishing FAHClient..."
-FAHClient --finish
-echo "Done"
+echo "Instructing FAHClient to finish all WUs, if any..."
+FAHClient --finish || exit 0
