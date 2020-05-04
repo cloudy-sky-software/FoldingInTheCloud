b56f86f87c91a53e76b6e7432012bdf13858d86b
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-04 09:41:30 -0700

    Fix default tagging.

--- tags.ts
+++ tags.ts
@@ -7,6 +7,22 @@ function getDefaultTags(): Record<string, string> {
     };
 }
 
+function isTaggable(type: string): boolean {
+    switch (type) {
+        case "aws:iam/instanceProfile:InstanceProfile":
+        case "aws:iam/policy:Policy":
+        case "aws:iam/rolePolicy:RolePolicy":
+        case "aws:iam/rolePolicyAttachment:RolePolicyAttachment":
+        case "aws:ec2/routeTableAssociation:RouteTableAssociation":
+        case "aws:cloudwatch/eventTarget:EventTarget":
+        case "aws:lambda/permission:Permission":
+        case "aws:s3/bucketNotification:BucketNotification":
+            return false;
+        default:
+            return true;
+    }
+}
+
 /**
  * registerDefaultTags registers a global stack transformation that merges a set
  * of tags with whatever was also explicitly added to the resource definition.
@@ -14,7 +30,7 @@ function getDefaultTags(): Record<string, string> {
 export function registerDefaultTags(): void {
     const autoTags = getDefaultTags();
     pulumi.runtime.registerStackTransformation((args) => {
-        if (args.props["tags"]) {
+        if (isTaggable(args.type)) {
             args.props["tags"] = { ...args.props["tags"], ...autoTags };
             return { props: args.props, opts: args.opts };
         }
