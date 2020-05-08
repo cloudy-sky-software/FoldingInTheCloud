15888d50fa9a5ba38ecd8f2c77f1f701a2f57085
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-07 18:44:22 -0700

    Create a new component to allow creating infra on either cloud.

--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 
 import * as ssh2 from "ssh2";
 import { ParsedKey } from "ssh2-streams";
 
-import { Ec2SpotInstance } from "./aws/ec2";
 import { execSync } from "child_process";
 import { registerDefaultTags } from "./tags";
-import { Events } from "./aws/events";
+import { SpotInstance } from "./spotInstance";
 
 // Get the config ready to go.
 const config = new pulumi.Config();
-const publicKey = config.require("publicKey");
 const privateKey = config.requireSecret("privateKey");
 const privateKeyPassphrase = config.get("privateKeyPassphrase") || "";
 
@@ -49,71 +46,4 @@ pulumi.all([fahUsername, fahPassKey, fahRemoteControlPass, fahAllowedIP]).apply(
     pulumi.log.info("Updated config.xml");
 });
 
-const bucket = new aws.s3.Bucket("fah-bucket", {
-    bucket: "fah-bucket",
-    serverSideEncryptionConfiguration: {
-        rule: {
-            applyServerSideEncryptionByDefault: {
-                sseAlgorithm: "AES256"
-            }
-        }
-    },
-    versioning: {
-        enabled: true
-    },
-});
-
-const ec2SpotInstance = new Ec2SpotInstance("fah-linux", {
-    /**
-     * When picking an instance type, be sure to also pick a region
-     * where the chance of interruption is low. Set the location using
-     * `pulumi config set aws:region <region>` to use that region.
-     * Use the Spot Instance Advisor to find a region for the instance type.
-     * https://aws.amazon.com/ec2/spot/instance-advisor/
-     */
-    instanceType: "g4dn.xlarge",
-    /**
-     * Max per-hour spot price is based on the price history for the instance
-     * per https://aws.amazon.com/ec2/spot/pricing/.
-     */
-    maxSpotPrice: "0.2",
-    /**
-     * Defined duration spot instances are less likely to be interrupted.
-     * And if they are interrupted, we won't incur charges for the hour
-     * in which it is interrupted. That sounds like a good deal.
-     */
-    // blockDurationMinutes: 180,
-
-    privateKey,
-    publicKey,
-    privateKeyPassphrase,
-
-    ingressRules: [
-        // For SSH access to the instance from the remote IP.
-        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: [pulumi.interpolate`${fahAllowedIP}/32`] },
-        // For SSH access to the instance from resources within the security group.
-        { protocol: "tcp", fromPort: 22, toPort: 22, self: true },
-        // To allow FAHControl on a remote IP to be able to connect to/control the FAHClient on the EC2 instance.
-        { protocol: "tcp", fromPort: 36330, toPort: 36330, cidrBlocks: [pulumi.interpolate`${fahAllowedIP}/32`] }
-    ],
-}, { dependsOn: bucket });
-
-export const bucketArn = bucket.arn;
-export const spotRequestId = ec2SpotInstance.spotRequest?.id;
-
-if (ec2SpotInstance && ec2SpotInstance.spotRequest) {
-    const zipFileName = "fah-scripts";
-    const events = new Events("fah-events", {
-        ec2Security: ec2SpotInstance.ec2Security,
-        spotInstanceRequest: ec2SpotInstance.spotRequest,
-        bucket,
-        zipFileName,
-    }, { dependsOn: ec2SpotInstance });
-
-    const bucketObject = new aws.s3.BucketObject("fah-scripts", {
-        bucket: bucket,
-        key: zipFileName,
-        serverSideEncryption: "AES256",
-        source: new pulumi.asset.FileArchive("./scripts")
-    }, { dependsOn: events });
-}
+export const spotInstance = new SpotInstance("fah-linux", {});
