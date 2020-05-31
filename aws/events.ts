b52b9a86fe63699579e012a3dc493519f80aca00
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-05-30 17:19:23 -0700

    Remove the need for pulumi.all in the Events component constructor.

--- aws/events.ts
+++ aws/events.ts
@@ -17,44 +17,45 @@ export class AwsEvents extends pulumi.ComponentResource {
         super("aws:events", name, undefined, opts);
 
         pulumi.all([args.spotInstanceRequest.id, args.bucket.bucket]).apply(([spotInstanceRequestId, bucketName]) => {
-            const handler = new EventsHandler("fah", {
-                ec2Security: args.ec2Security,
-                bucketName: bucketName,
-                spotInstanceRequestId: spotInstanceRequestId,
-                zipFilename: args.zipFileName,
-            }, { parent: this });
 
-            if (!handler.callbackFunction) {
-                return;
-            }
-
-            args.bucket.onObjectCreated("fah-object-created", handler.callbackFunction, undefined, { parent: this });
-
-            const instanceRunningEvent = new aws.cloudwatch.EventRule(`${name}-inst-running`, {
-                description: "Event rule for Spot Instance request fulfillment.",
-                eventPattern: JSON.stringify({
-                    "source": ["aws.ec2"],
-                    "detail-type": ["EC2 Instance State-change Notification"],
-                    "detail": {
-                        "state": ["running"]
-                    }
-                }),
-            }, { parent: this });
-
-            instanceRunningEvent.onEvent(`${name}-inst-running-sub`, handler.callbackFunction, undefined, { parent: this });
-
-            const eventRule = new aws.cloudwatch.EventRule(`${name}-interruption`, {
-                description: "Event rule for Spot Instance request fulfillment.",
-                eventPattern: JSON.stringify({
-                    "source": ["aws.ec2"],
-                    "detail-type": ["EC2 Spot Instance Interruption Warning"]
-                }),
-            }, { parent: this });
-
-            eventRule.onEvent(`${name}-interruption-sub`, handler.callbackFunction, undefined, { parent: this });
-
-            this.registerOutputs({});
         });
+        const handler = new EventsHandler("fah", {
+            ec2Security: args.ec2Security,
+            bucketName: args.bucket.bucket,
+            spotInstanceRequestId: args.spotInstanceRequest.id,
+            zipFilename: args.zipFileName,
+        }, { parent: this });
+
+        if (!handler.callbackFunction) {
+            return;
+        }
+
+        args.bucket.onObjectCreated("fah-object-created", handler.callbackFunction, undefined, { parent: this });
+
+        const instanceRunningEvent = new aws.cloudwatch.EventRule(`${name}-inst-running`, {
+            description: "Event rule for Spot Instance request fulfillment.",
+            eventPattern: JSON.stringify({
+                "source": ["aws.ec2"],
+                "detail-type": ["EC2 Instance State-change Notification"],
+                "detail": {
+                    "state": ["running"]
+                }
+            }),
+        }, { parent: this });
+
+        instanceRunningEvent.onEvent(`${name}-inst-running-sub`, handler.callbackFunction, undefined, { parent: this });
+
+        const eventRule = new aws.cloudwatch.EventRule(`${name}-interruption`, {
+            description: "Event rule for Spot Instance request fulfillment.",
+            eventPattern: JSON.stringify({
+                "source": ["aws.ec2"],
+                "detail-type": ["EC2 Spot Instance Interruption Warning"]
+            }),
+        }, { parent: this });
+
+        eventRule.onEvent(`${name}-interruption-sub`, handler.callbackFunction, undefined, { parent: this });
+
+        this.registerOutputs({});
     }
 }
 
