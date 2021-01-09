cd10418804f1dd7a0de0b4a918a8a0446df383d2
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2021-01-09 13:31:12 -0800

    Fix some more eslint errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+        "node": true,
+        "es2021": true,
+        "browser": false
     },
     "extends": [
         "eslint:recommended",
